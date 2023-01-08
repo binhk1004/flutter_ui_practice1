2f378089408cfad7badfba432fc4ec44e100cd36
binhk1004 <binhk1004@naver.com>
2023-01-08 22:48:48 +0900

    Transiton

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_practice1/nextpage.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,28 +9,106 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
+    return const CupertinoApp(
+      home: home(),
+    );
+  }
+}
+
+class home extends StatelessWidget {
+  const home({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
               const SizedBox(
-                height: 300,
+                height: 100,
               ),
-              const Icon(
-                Icons.bus_alert,
+              SizedBox(
+                height: 400,
+                width: 400,
+                child: Image.asset('assets/image/IMG_0021.jpg'),
               ),
               Column(
                 children: const [
                   Text(
-                    'hello!',
+                    'Better Travle With',
                     style: TextStyle(
                       color: Colors.black,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
                     ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    'better feels',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
                 ],
-              )
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Column(
+                children: const [
+                  Text(
+                    'Travel tupseen lorem ipsum dolor sit her pontpy ento amet consectetur. Cursus auctor vimin vitae porta.',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const NextPage()),
+                  );
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: const Color(0xFFCFD428),
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ),
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 3,
+                      )),
+                  width: 300,
+                  child: const Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text(
+                      'Get Started',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
