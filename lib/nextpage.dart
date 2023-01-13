cc79acdc357f0074a05f4ab6bf0a5f0df8f51653
binhk1004 <binhk1004@naver.com>
2023-01-14 01:47:25 +0900

    SearchBox

--- lib/nextpage.dart
+++ lib/nextpage.dart
@@ -5,8 +5,50 @@ class NextPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.red,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            Container(
+              alignment: Alignment.bottomLeft,
+              child: const Text(
+                'Travel',
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Search all...',
+                labelStyle: const TextStyle(
+                  color: Color(0xFF312466),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(13),
+                ),
+                filled: true,
+                isDense: true,
+                contentPadding: const EdgeInsets.all(13),
+                suffixIcon: const Icon(
+                  Icons.search_rounded,
+                  size: 30,
+                  color: Color(0xFF312466),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
