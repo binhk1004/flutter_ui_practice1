e3897e0dba89dc80c7aa8fe61814bcd38f510ff3
binhk1004 <binhk1004@naver.com>
2023-01-16 22:53:17 +0900

    Created Container

--- lib/nextpage.dart
+++ lib/nextpage.dart
@@ -46,6 +46,105 @@ class NextPage extends StatelessWidget {
                 ),
               ),
             ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: const [
+                Text(
+                  'Popular places',
+                  style: TextStyle(
+                    color: Color(0xFF141352),
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color(0xFF1A175E),
+                    offset: Offset(5, 5),
+                  ),
+                ],
+              ),
+              height: 250,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/image/IMG_0021.jpg',
+                          width: 100,
+                          height: 100,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: const [
+                                Icon(Icons.tour),
+                                Text("Gyeongbokgung"),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              children: const [
+                                Icon(Icons.pin_drop),
+                                Text("South Korea"),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Container(
+                              width: 130,
+                              height: 60,
+                              decoration: BoxDecoration(
+                                border: Border.all(color: Colors.black),
+                                color: const Color(0xFFd0d328),
+                                borderRadius: BorderRadius.circular(13),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Color(0xFF1A175E),
+                                    offset: Offset(3, 3),
+                                  ),
+                                ],
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [
+                                  Text('visit now'),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
